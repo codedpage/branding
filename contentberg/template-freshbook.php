c3a542fa0ba54ae5da57fbcdd072eeac571dad69
manoj <manoj@brandingrain.com>
2024-02-15 20:06:57 +0530

    removed

--- contentberg/template-freshbook.php
+++
@@ -1,4 +0,0 @@
-<?php /* Template Name: Freshbook Template */ 
-echo "<pre>";
-print_r($_REQUEST);
-?>
--- contentberg/template-unplug.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Template Name: Unplug
- * 
- * Static Find Addiction Treatment Center with search functionality
- * 
- */
-while ( have_posts() ){ the_post(); ?>
-<?php the_content();} ?>
