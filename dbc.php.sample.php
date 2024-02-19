aad07a19d9f3193ec7ed53d13fe9bc64150cb682
manoj <manoj@brandingrain.com>
2024-02-19 23:46:12 +0530

    sample

--- dbc.php.sample.php
+++ dbc.php.sample.php
@@ -5,3 +5,4 @@ $database = 'xxx';
 $port = 3306;
 $mysqli = new mysqli('xxxx', $user, $password, $database, $port);
 if ($mysqli->connect_error) { die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);}
+echo '<p>Connection OK '. $mysqli->host_info.'</p>';
