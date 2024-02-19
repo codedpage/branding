c568e1a2a4295f2f6a6d1c91c8a852cf3d0931f5
manoj <manoj@brandingrain.com>
2024-02-19 23:29:54 +0530

    connection

---
+++ dbc.php.sample.php
@@ -0,0 +1,7 @@
+<?php
+$user = 'xxx';
+$password = 'xxx';
+$database = 'xxx';
+$port = 3306;
+$mysqli = new mysqli('xxxx', $user, $password, $database, $port);
+if ($mysqli->connect_error) { die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);}
