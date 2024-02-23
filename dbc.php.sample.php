bf8d0104b9eb70f1f071a16ac2ed57a55acc8f03
manoj <manoj@brandingrain.com>
2024-02-23 05:51:30 +0530

    dbc updated

--- dbc.php.sample.php
+++ dbc.php.sample.php
@@ -3,6 +3,10 @@ $user = 'xxx';
 $password = 'xxx';
 $database = 'xxx';
 $port = 3306;
-$mysqli = new mysqli('xxxx', $user, $password, $database, $port);
+
+//$server = 'xx.xx.xx.rearmirror';   //master
+$server = 'xx.xx.file.redrose';      //slave
+
+$mysqli = new mysqli($server, $user, $password, $database, $port);
 if ($mysqli->connect_error) { die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);}
 echo '<p>Connection OK '. $mysqli->host_info.'</p>';
